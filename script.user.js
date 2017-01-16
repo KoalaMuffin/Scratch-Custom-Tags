3894b1fa2da0e644ab93901dd6710e23d04feb85
jamescu5 <jcu123123@gmail.com>
2017-01-16 10:21:36 -0500

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -31,10 +31,10 @@
     $('#project-tags').click(function() {
       if ($('#project-tags').is(':hover')) {
         var tag = encodeURIComponent(prompt("Tag content?"));
-          $.ajax({
-            type: "PUT",
-            url: "https://scratch.mit.edu/site-api/tags/project/" + ID + "/add/?names=" + tag
-          });
+        $.ajax({
+          type: "PUT",
+          url: "https://scratch.mit.edu/site-api/tags/project/" + ID + "/add/?names=" + tag
+        });
         location.reload();
       }
     });
