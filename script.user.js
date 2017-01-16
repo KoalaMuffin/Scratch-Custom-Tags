b1142caf3175feb15bb9c7f49ccdd25e74ccf518
jamescu5 <jcu123123@gmail.com>
2017-01-16 10:28:58 -0500

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,4 +1,3 @@
-
 // ==UserScript==
 // @name         Custom Scratch Tags
 // @namespace    http://tampermonkey.net/
@@ -32,10 +31,12 @@
     $('#project-tags').click(function() {
       if ($('#project-tags').is(':hover')) {
         var tag = encodeURIComponent(prompt("Tag content?"));
+        if (tag !== null) {
         $.ajax({
           type: "PUT",
           url: "https://scratch.mit.edu/site-api/tags/project/" + ID + "/add/?names=" + tag
         });
+        }
         location.reload();
       }
     });
